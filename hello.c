03f56272746934e147a6798ba4e33236dc9c3b7f
ericstephens <e.a.stephens@me.com>
2016-01-30 16:52:18 -0500

    this is another change. added function but also had to use the stage command before committing. didn't have to do that from the command line

--- hello.c
+++ hello.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+int compute(int a, int b) {
+  return a+(2*b);
+}
+
 int main(int argc, char **argv) {
   printf("Hi again\n");
   printf("this is another test. want to stress it out\n");
-  printf("hi emma\n");
+  printf("%d\n",compute(3,2));
 }
