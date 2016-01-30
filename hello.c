3e84a0631f39aa9648a6dca9796b079526538cad
ericstephens <e.a.stephens@me.com>
2016-01-30 16:37:42 -0500

    hi

--- hello.c
+++ hello.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 
-/*
-
-	This is a comment
-	$Id$
-*/
-
-int main (int argc, char** argv) {
-	printf("hi again\n");
-	printf("this is another test. want to stress it out\n");
+int main(int argc, char **argv) {
+  printf("hi again\n");
+  printf("this is another test. want to stress it out\n");
+  printf("hi emma\n");
 }
