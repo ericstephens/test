9ca37e00f7dca76b8ba373b7d8b2de3e7ab4781b
ericstephens <e.a.stephens@me.com>
2016-01-30 16:39:31 -0500

    this is a test

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  printf("hi again\n");
+  printf("Hi again\n");
   printf("this is another test. want to stress it out\n");
   printf("hi emma\n");
 }
