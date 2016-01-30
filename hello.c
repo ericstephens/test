ca93bfe9a8408921337178b465123336ee54eff5
ericstephens <e.a.stephens@me.com>
2016-01-30 17:08:22 -0500

    altered first message

--- hello.c
+++ hello.c
@@ -5,7 +5,7 @@ int compute(int a, int b) {
 }
 
 int main(int argc, char **argv) {
-  printf("Hi again\n");
+  printf("Hi Again\n");
   printf("this is another test. want to stress it out\n");
   printf("%d\n",compute(3,2));
 }
